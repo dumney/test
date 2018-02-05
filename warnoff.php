5a9440bfd62fc5ab8d795f14b21e98cfd4d518ad
Dumney <eluyin@gmail.com>
2018-02-05 22:22:34 +0200

    Update warnoff.php

--- warnoff.php
+++ warnoff.php
@@ -1,8 +1,8 @@
 <!-- Отключение навязчивого предупреждения -->
-<form action="warnoff.php">
-<input type="submit" name="doGo" value="Click!">
-</form>
-<?php
+ <form action="warnoff.php">
+   <input type="submit" name="doGo" value="Click!">
+ </form>
+   <?php
 // В массиве $_REQUEST всегда содержатся пришедшие из формы данные.
-if (@$_REQUEST['doGo']) echo "Вы нажали кнопку!";
-?>
+     if (@$_REQUEST['doGo']) echo "Вы нажали кнопку!";
+ ?>
