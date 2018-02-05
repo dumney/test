94f9dc0f0c2c061d3a74339e3f2900af25710e28
Dumney <eluyin@gmail.com>
2018-02-05 22:21:48 +0200

    Update and rename index.php to request.php

---
+++ request.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+   <head>
+      <title>Документ с формой</title>
+      <meta charset='utf-8'>
+    </head>
+       <body><!-- Навязчивые предупреждения -->
+         <form action="warn.php">
+            <input type="submit" name="doGo" value="Click!">
+          </form>
+             <?php ## В массиве $_REQUEST всегда содержатся пришедшие из формы данные. 
+                if ($_REQUEST['doGo']) echo "Вы нажали кнопку!";?>
+	</body>
+</html>
