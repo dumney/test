30c67e1809f95b67df6198ee688e7c2a4ade2c64
Dumney <eluyin@gmail.com>
2018-02-05 19:41:14 +0200

    Create form.php

---
+++ form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+  <html lang="ru">
+    <head>
+      <title>Вывод параметров командной строки</title>
+      <meta charset='utf-8'>
+    </head>
+  <body>
+    <?= "Данные из командной строки: {$_SERVER['QUERY_STRING']}"; ?>
+  </body>
+ </html>
+ <?php
+  inlcude "hello.php";
+     ?>
